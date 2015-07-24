9dc9b5847eb7f59b78ad95579c54ce9ebf12ea50
mdaugustine <michael.daugustine@gmail.com>
2015-07-24 15:16:07 -0500

    Read single file by tab. Need to extract date, app, and amount

---
+++ adr-financials-report/adr-financials-report/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open System.IO
+open System
+
+let filePath = "test.txt"
+
+let readLines = seq {
+    use sr = new StreamReader (filePath)
+    while not sr.EndOfStream do
+        //for tab in sr.ReadLine().Split([|'\t'|]) do
+        yield sr.ReadLine().Split([|'\t'|])
+}
+
+readLines |> Seq.iter(fun x -> x |> Seq.iter(fun y -> printfn "%s" y))
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
