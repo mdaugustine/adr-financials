c67d66a2a7918508d5591f7c2c89f34bc9ddf1b1
mdaugustine <michael.daugustine@gmail.com>
2015-07-27 16:25:24 -0500

    total sales

--- adr-financials-report/adr-financials-report/Program.fs
+++ adr-financials-report/adr-financials-report/Program.fs
@@ -36,20 +36,75 @@ let values = salesRecord.fromFile "test.txt"
 *)
 
 let dir = new DirectoryInfo(@"C:\Users\Mdaugustine\Documents\adr-financials\adr-sales")
-type SalesReport = CsvProvider<"test.txt", Separators="\t", IgnoreErrors=true>
+type SalesReport = CsvProvider<Sample = "test.txt", Separators="\t", InferRows=0, IgnoreErrors=true, HasHeaders=true>
+
+let outFile = new StreamWriter("testing.txt")
+
+let files = dir.GetFiles()
+
+let currencies (report:SalesReport)= 
+    report.Rows
+    |> Seq.map (fun row -> row.``Customer Currency``)
+    |> Seq.head
+
+let sumSales (report:SalesReport) =
+    report.Rows
+    |> Seq.map (fun row -> row.``Extended Partner Share``)
+    |> Seq.filter (fun elem -> not (Double.IsNaN (float(elem))))
+    |> Seq.sum
 
 let sales = 
-    [for file in dir.GetFiles() do
+    files
+    |> Seq.map (fun f -> SalesReport.Load(f.FullName))
+    |> Seq.map (fun report -> sumSales report)
+
+let currency =
+    files
+    |> Seq.map (fun f -> SalesReport.Load(f.FullName))
+    |> Seq.map (fun report -> currencies report)
+
+(*
+let sales =
+    [for file in files do
         let salesReport = SalesReport.Load(file.FullName)
-        let totalSales =
-            salesReport.Rows
-            |> Seq.map (fun row -> row.``Extended Partner Share``)
-            |> Seq.filter (fun elem -> not (Double.IsNaN (float(elem))))
-            |> Seq.sum
-        yield totalSales]
+        //let sum = sumSales salesReport
+        //outFile.WriteLine(sprintf "%f and %s" sum file.Name)
+        yield salesReport]
+    |> Seq.map (fun report -> sumSales report)
+*)
 
 for sale in sales do
-    printfn "%f" sale
+    outFile.WriteLine(sprintf "%f" sale)
+
+//for c in currency do
+//        printfn "%s" c
+//for currency in currencies do
+//    outFile.WriteLine(sprintf "%s" currency.``Customer Currency``)
+
+
+(*
+let first (report:SalesReport) = 
+    report.Rows
+    |> Seq.map (fun row -> row.``Customer Currency``)
+    |> Seq.head
+
+let currencies =
+    [for file in dir.GetFiles() do
+        let salesReport = SalesReport.Load(file.FullName)
+        yield salesReport]
+        |> Seq.map (fun report -> first report)
+
+for currency in currencies do
+    printfn "%s" currency
+let salesReport2 = SalesReport.Load("test.txt")
+*)
+//let first = salesReport2.Rows |> Seq.head
+//let currency = first.``Customer Currency``
+
+//printfn "%s" currency
+
+//for sale in sales do
+//    printfn "%f" sale
 
 (*
 let salesReport = SalesReport.Load("test.txt")
